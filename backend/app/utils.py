e4b78fdbb1cf0567902cd1f1d55a154a6702e27e
Cercel, Cristian <cristian.cercel@outlook.com>
2026-02-15 20:19:31 -0500

    started court

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,3 +1,5 @@
+from typing import List
+import plotly.graph_objects as go
 from sqlalchemy.orm import Session
 from app.models import Match, PlayerStats
 from app.schemas import PlayerBase, MatchResponse, MatchType, TeamColor
@@ -55,3 +57,67 @@ def update_player_stats(
     else:
         stats.losses += 1
         stats.streak = 0
+
+
+def plot_empty_court() -> go.Figure:
+    court_width = 9
+    court_length = 18
+    
+    fig = go.Figure()
+
+    # 1. Add Court Boundary (Closed loop)
+    fig.add_trace(go.Scatter(
+        x=[0, court_width, court_width, 0, 0],
+        y=[0, 0, court_length, court_length, 0],
+        mode='lines',
+        line=dict(color='darkred', width=4),
+        name='Boundary'
+    ))
+
+    # 2. Centerline
+    fig.add_trace(go.Scatter(
+        x=[0, court_width],
+        y=[court_length / 2, court_length / 2],
+        mode='lines',
+        line=dict(color='black', width=3, dash='dash'),
+        name='Center Line'
+    ))
+
+    # 3. Attack Lines (3 meters from center)
+    # Blue Team (Bottom)
+    fig.add_trace(go.Scatter(
+        x=[0, court_width],
+        y=[(court_length / 2) - 3, (court_length / 2) - 3],
+        mode='lines',
+        line=dict(color='darkred', width=2, dash='dash'),
+        opacity=0.5,
+        name='Attack Line (Blue Team)'
+    ))
+    
+    # Red Team Side (Top)
+    fig.add_trace(go.Scatter(
+        x=[0, court_width],
+        y=[(court_length / 2) + 3, (court_length / 2) + 3],
+        mode='lines',
+        line=dict(color='darkred', width=2, dash='dash'),
+        opacity=0.5,
+        name='Attack Line (Red Team)'
+    ))
+
+    # 4. Styling the Layout
+    fig.update_layout(
+        plot_bgcolor='bisque',
+        paper_bgcolor='bisque',
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False, range=[-1, court_width + 1]),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False, range=[-1, court_length + 1]),
+        width=500,
+        height=800,
+        showlegend=False,
+        margin=dict(l=20, r=20, t=20, b=20)
+    )
+
+    return fig
+
+
+def plot_volleyball_court(blue_team: List[str], red_team: List[str]):
+
